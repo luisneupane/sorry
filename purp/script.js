7fbcc53afaceb325b6890300d4dba980c73fa049
prajwol <neupaneluis7@gmail.com>
2026-02-14 21:56:28 +0545

    Add files via upload

---
+++ purp/script.js
@@ -0,0 +1,87 @@
+console.clear();
+
+setTimeout(() => {
+  const loveEl = document.getElementById("love");
+  loveEl.classList.add("c"); // add class "c"
+}, 5000);
+
+/* THREE.JS SETUP */
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 5000);
+camera.position.z = 500;
+
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setPixelRatio(window.devicePixelRatio > 1 ? 2 : 1);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+/* CONTROLS */
+const controlsWebGL = new THREE.OrbitControls(camera, renderer.domElement);
+
+/* PARTICLES */
+const tl = gsap.timeline({ repeat: -1, yoyo: true });
+
+const path = document.querySelector("path");
+const length = path.getTotalLength();
+const vertices = [];
+
+for (let i = 0; i < length; i += 0.1) {
+  const point = path.getPointAtLength(i);
+  const vector = new THREE.Vector3(point.x, -point.y, 0);
+  vector.x += (Math.random() - 0.5) * 30;
+  vector.y += (Math.random() - 0.5) * 30;
+  vector.z += (Math.random() - 0.5) * 70;
+  vertices.push(vector);
+
+  tl.from(vector, {
+    x: 600 / 2,
+    y: -552 / 2,
+    z: 0,
+    ease: "power2.inOut",
+    duration: "random(2, 5)"
+  }, i * 0.002);
+}
+
+const geometry = new THREE.BufferGeometry().setFromPoints(vertices);
+const material = new THREE.PointsMaterial({ color: 0xee5282, blending: THREE.AdditiveBlending, size: 3 });
+const particles = new THREE.Points(geometry, material);
+
+particles.position.x -= 600 / 2;
+particles.position.y += 552 / 2;
+scene.add(particles);
+
+gsap.fromTo(scene.rotation, { y: -0.2 }, { y: 0.2, repeat: -1, yoyo: true, ease: 'power2.inOut', duration: 3 });
+
+/* RENDER LOOP */
+function render() {
+  requestAnimationFrame(render);
+  geometry.setFromPoints(vertices);
+  renderer.render(scene, camera);
+}
+requestAnimationFrame(render);
+
+/* HANDLE WINDOW RESIZE */
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener("resize", onWindowResize, false);
+
+/* TYPEWRITER EFFECT */
+window.addEventListener("DOMContentLoaded", () => {
+  const textEl = document.getElementById("text");
+  const text = textEl.innerText;
+  textEl.innerText = "";
+  let i = 0;
+
+  function typeWriter() {
+    if (i < text.length) {
+      textEl.innerText += text.charAt(i);
+      i++;
+      setTimeout(typeWriter, 100);
+    }
+  }
+
+  typeWriter();
+});
